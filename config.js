e48536283bb249435da2cea17a4a8cf5818cd255
Binuka Lasad Udayanga <binukaudayanga3@gmail.com>
2025-12-30 20:53:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "ZmJQ0JwT#yHaqkMtzQOkc7DwaCaULcDomkkthuJkcVytgMf24EUI",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
